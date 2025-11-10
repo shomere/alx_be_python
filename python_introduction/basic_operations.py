905ec279484a0ac3cf8a354d1cb6a1942529e1b1
shomere <s.homere@alustudent.com>
2025-11-10 16:12:01 +0200

    basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,27 @@
+# additing values in python
+
+number_1 = 10
+number_2 = 5
+
+result = number_1 + number_2
+
+print(result)
+
+# subtration of values in python
+
+number_1 = 10
+number_2 = 5
+
+result = number_1 - number_2
+
+print(result)
+
+# Multiplicaton of values in python
+
+numbernumber_1 = 10
+number_2 = 5
+
+result = number_1 * number_2
+
+print(result)
+

d4b91878f4c7137c52290f106a341be968afd1b7
shomere <s.homere@alustudent.com>
2026-02-25 12:41:41 +0200

    adding operators

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -2,11 +2,11 @@ weather = input("What's the weather like today? (sunny, rainny, cold): ")
 
 # provide the clothing recommendation
 
-if weather is "sunny":
+if weather == "sunny":
     print("Wear a T-shirt  and sunglasses.")
-elif weather is "rainny":
+elif weather == "rainny":
     print("Don't forget the Umbrella and Raincoat.")
-elif waether is "cold":
+elif waether == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")

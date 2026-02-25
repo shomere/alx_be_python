333c604727e05be4e8950d8f50eb2f438c5a2bf3
shomere <s.homere@alustudent.com>
2026-02-25 13:53:41 +0200

    match_case

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# prompt the user input
+num1 = input("Enter the First number: ")
+num2 = input("Enter the second number: ")
+operation = input("Choose the operation(+, -, *, /): ")
+
+# perform the calculation with matchcase
+
+match operation:
+    case "+":
+        result = num1 + num2
+        print(f"The result is {result}.")
+    case "-":
+        result = num1 - num2
+        print(f"The result is {result}.")
+    case "*":
+        result = num1 * num2
+        print(f"The result is {result}.")
+    case "/":
+        if num2 == 0:
+            print("Cannot divide by 0.")
+        else:
+            result = num1 / num2
+            print (f"The result is {result}.")
+    case _:
+        print("Invalid operaton selected")

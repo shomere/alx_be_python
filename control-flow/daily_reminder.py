00aff7a6b4e8696b68eef4e5eee0e400568918c1
shomere <s.homere@alustudent.com>
2026-02-26 11:14:28 +0200

    daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,30 +1,26 @@
 #!/usr/bin/env python3
 
-# prompt the user for single task
-
+# Prompt for a single task
 task = input("Enter your task: ")
-priority = input("Priority(high/medium/low): ")
-time_bound = input("Is it time-bound?(yes/no): ")
+priority = input("Priority (high/medium/low): ")
+time_bound = input("Is it time-bound? (yes/no): ")
 
-#process the task based on priority and time sensitivity
-
+# Process the task based on priority and time sensitivity
 match priority:
     case "high":
-        reminder = f"{task} is high priority task"
+        reminder = f"'{task}' is a high priority task"
     case "medium":
-        reminder = f"{task} is medium priority task"
+        reminder = f"'{task}' is a medium priority task"
     case "low":
-        reminder = f"{task} is low priority task"
-    case -:
-        reminder = f"{task} is unknown priority"
+        reminder = f"'{task}' is a low priority task"
+    case _:
+        reminder = f"'{task}' is a task with unknown priority"
 
-# modify reminder if time-bound
-
-if time-bound == "yes":
-    reminder += "that require immediate attention today!"
+# Modify reminder if time-bound
+if time_bound == "yes":
+    reminder += " that requires immediate attention today!"
     print(f"Reminder: {reminder}")
 else:
     print(f"Note: {reminder}. Consider completing it when you have free time.")
 
 print("\nWell done on completing this project! Let the world hear about this milestone achieved.")
-
